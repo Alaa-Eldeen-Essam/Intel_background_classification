f90a8f75ec6f9bda8bb130668b08b9dcd14dd13a
Alaa1299 <axxxaa990@gmail.com>
2025-10-23 16:58:08 +0300

    update main

--- src/main.py
+++ src/main.py
@@ -315,7 +315,7 @@ async def general_exception_handler(request: Request, exc: Exception):
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "src.main:app",
         host=settings.HOST,
         port=settings.PORT,
         reload=settings.RELOAD,
